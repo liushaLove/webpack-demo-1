2c4e8ad5df96f5479061550766c749e3a0374bda
liushaLove <1270842602@qq.com>
2020-12-15 23:20:04 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,3 +1,2 @@
 import CSS from './x.css';
-console.log(CSS.toString());
 export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name][contenthash].js'
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: "xiaobangsky",
-    template: "./src/assets/index.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "xiaobangsky",
+      template: "./src/assets/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder:false
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath:'../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
       },
     ],
   }
