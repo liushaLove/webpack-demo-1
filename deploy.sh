018446ff75c10797ca8794438bff85686e776f6f
liushaLove <1270842602@qq.com>
2020-12-16 21:46:03 +0800

    upd

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 yarn build &&
 git checkout gh-pages &&
-rm -rf *.html *.jpg *.sh *.js *json yarn.lock &&
+rm -rf *.html *.js *.css *.jpg  &&
 mv dist/* ./ && 
 rm -rf dist;
 git add . && 
 git commit -m 'update' &&
-git push
+git push && 
+git checkout -
